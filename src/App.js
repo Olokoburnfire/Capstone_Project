09d9343af33fc44d3c72a08dd7886f523bb36ee6
Olokodana AbdulKabir <Olokodanaabdulkabir@gmail.com>
2023-05-18 00:20:27 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import './App.css';
+import React from "react";
+import Home from "./routes/Home";
+import Reservations from "./routes/Reservations";
+
+import {Route, Routes} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1> Homepage</h1>
-    </div>
+    <>
+      <Routes>
+        <Route path = "/" element = {<Home />}/>
+        <Route path = "/reservations" element = {<Reservations />}/>
+      </Routes>
+    </>
   );
 }
 
